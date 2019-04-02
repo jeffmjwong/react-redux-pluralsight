78ea9da8f73b3bba70e718d9712ced717451cdfd
Jeff Wong <jeffmjwong@gmail.com>
2019-04-02 21:52:06 +1000

    Fix typo

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,8 @@
-import { combinedReducers } from 'redux';
+import { combineReducers } from 'redux';
 
 import courses from './courseReducer';
 
-const rootReducer = combinedReducers({
+const rootReducer = combineReducers({
   courses,
 });
 
