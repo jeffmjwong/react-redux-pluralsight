cb43768d40611527030a73a2f68b9cdabe439a19
Jeff Wong <jeffmjwong@gmail.com>
2019-04-02 17:47:51 +1000

    Handle change and submit in courses page

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
 
 class CoursesPage extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      course: {
-        title: ''
-      }
-    };
+  state = {
+    course: {
+      title: ''
+    }
   }
 
   handleChange = (evt) => {
+    const course = { ...this.state.course, title: evt.target.value };
+    this.setState({ course });
+  }
 
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+    alert(this.state.course.title);
   }
 
   render() {
     const { course } = this.state;
 
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
         <input
