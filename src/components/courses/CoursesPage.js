b4ede03f0a0b144744040041d228e8f57a7a9dd4
Jeff Wong <jeffmjwong@gmail.com>
2019-04-03 20:35:51 +1000

    Remove presentation jsx in coursesPage

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,42 +5,17 @@ import * as courseActions from '../../redux/actions/courseActions';
 import { bindActionCreators } from 'redux';
 
 class CoursesPage extends Component {
-  state = {
-    course: {
-      title: ''
-    }
-  }
-
-  handleChange = (evt) => {
-    const course = { ...this.state.course, title: evt.target.value };
-    this.setState({ course });
-  }
-
-  handleSubmit = (evt) => {
-    evt.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  }
-
   render() {
-    const { course } = this.state;
-
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={course.title}
-        />
-        <input type="submit" value="Save" />
 
         {
           this.props.courses.map((course) => (
             <div key={course.title}>{ course.title }</div>
           ))
         }
-      </form>
+      </>
     );
   }
 }
