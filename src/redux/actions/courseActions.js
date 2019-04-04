5cb20ac4e0090d73d62e1bf6dc75117b29522c77
Jeff Wong <jeffmjwong@gmail.com>
2019-04-04 17:26:29 +1000

    Add saveCourse thunk and create/update success

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,19 +1,33 @@
 import * as types from './actionTypes';
 import * as courseAPI from '../../api/courseApi';
 
-export const createCourse = (course) => {
-  return { type: types.CREATE_COURSE, course };
-}
-
 export const loadCoursesSuccess = (courses) => {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
-export const loadCourses = () => {
-  return (dispatch) => {
-    return courseAPI
-      .getCourses()
-      .then(courses => dispatch(loadCoursesSuccess(courses)))
-      .catch(error => { throw error });
-  };
+export const createCourseSuccess = (course) => {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export const updateCourseSuccess = (course) => {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export const loadCourses = () => dispatch => {
+  return courseAPI
+    .getCourses()
+    .then(courses => dispatch(loadCoursesSuccess(courses)))
+    .catch(error => { throw error });
+}
+
+//eslint-disable-next-line no-unused-vars
+export const saveCourse = course => (dispatch, getState) => {
+  return courseAPI
+    .saveCourse(course)
+    .then(savedCourse => {
+      course.id ?
+        dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    })
+    .catch(error => { throw error });
 }
