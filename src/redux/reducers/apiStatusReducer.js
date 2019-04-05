184df45c12fef1f7e6e87827ff93f643b04930cc
Jeff Wong <jeffmjwong@gmail.com>
2019-04-05 21:35:40 +1000

    Handle api call error in api reducer

--- src/redux/reducers/apiStatusReducer.js
+++ src/redux/reducers/apiStatusReducer.js
@@ -8,7 +8,10 @@ const actionTypeEndsInSuccess = type => {
 const apiStatusReducer = (state = initialState.apiCallsInProgress, action) => {
   if (action.type === types.BEGIN_API_CALL) {
     return state + 1;
-  } else if (actionTypeEndsInSuccess(action.type)) {
+  } else if (
+    action.type === types.API_CALL_ERROR ||
+    actionTypeEndsInSuccess(action.type)
+  ) {
     return state - 1;
   }
 
