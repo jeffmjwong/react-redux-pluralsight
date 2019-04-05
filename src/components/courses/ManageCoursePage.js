2cad365d1a23050cfaf236bbf1e509d023ad7f53
Jeff Wong <jeffmjwong@gmail.com>
2019-04-05 21:36:10 +1000

    Manage client side validation

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -45,13 +45,34 @@ const ManageCoursePage = ({
     });
   };
 
+  const formIsValid = () => {
+    const { title, authorId, category } = course;
+    const errors = {};
+
+    if (!title) errors.title = 'Title is required';
+    if (!authorId) errors.author = 'Author is required';
+    if (!category) errors.category = 'Category is required';
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
+
   const handleSave = (evt) => {
     evt.preventDefault();
+
+    if (!formIsValid()) return;
+
     setSaving(true);
-    saveCourse(course).then(() => {
-      toast.success('Course saved.');
-      history.push('/courses');
-    });
+    saveCourse(course)
+      .then(() => {
+        toast.success('Course saved.');
+        history.push('/courses');
+      })
+      .catch(error => {
+        setSaving(false);
+        setErrors({ onSave: error.message })
+      });
   };
 
   return (
