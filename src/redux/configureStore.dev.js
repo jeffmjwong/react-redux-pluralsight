60494d312c7206cce7efeb9c2f520202fb55d721
Jeff Wong <jeffmjwong@gmail.com>
2019-04-07 14:27:35 +1000

    Add prod and dev configureStore.js

---
+++ src/redux/configureStore.dev.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
+
+import rootReducer from './reducers';
+
+const configureStore = (initialState) => {
+  // add support for Redux dev tools
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+  return createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())),
+  );
+};
+
+export default configureStore;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,6 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-import rootReducer from './reducers';
-
-const configureStore = (initialState) => {
-  // add support for Redux dev tools
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())),
-  );
-};
-
-export default configureStore;
+// Use CommonJS require below so we can dynamically import during build-time.
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
+}
